72bbed34a397f7a454c5d208bab5f9438865abd8
AlharthAlhajHussein <alharth.alhaj.hussein@gmail.com>
2026-01-17 11:16:12 +0300

    add schema for the requests

---
+++ src/routes/schemes/nlp.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class PushRequest(BaseModel):
+    do_reset: Optional[int] = 0
+    
+
+class SearchRequest(BaseModel):
+    query_text: str
+    top_k: Optional[int] = 5
+    
