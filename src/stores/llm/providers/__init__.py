ebb1b1c40f767115a84d16f882dd56bfa1e5f89a
AlharthAlhajHussein <alharth.alhaj.hussein@gmail.com>
2026-01-21 20:57:54 +0300

    add ollama provider code

--- src/stores/llm/providers/__init__.py
+++ src/stores/llm/providers/__init__.py
@@ -1,3 +1,4 @@
 from .openai_provider import OpenAIProvider
 from .gemini_provider import GeminiProvider
 from .hugging_face_provider import HuggingFaceProvider
+from .ollama_provider import OllamaProvider
