9f975d1eaf68aa57c0faef8b34785c390a6048eb
AlharthAlhajHussein <alharth.alhaj.hussein@gmail.com>
2026-02-20 22:40:58 +0300

    Refactor ChunkModel methods for consistent return statements; add get_total_chunks_count method for project chunk count retrieval

--- src/models/chunk_model.py
+++ src/models/chunk_model.py
@@ -31,7 +31,7 @@ class ChunkModel(BaseDataModel):
     async def get_chunk(self, chunk_id: int) -> Any:
         async with self.db_client() as session:
             chunk = await session.execute(select(DataChunk).where(DataChunk.chunk_id == chunk_id))
-            return chunk.scalar_one_or_none()
+        return chunk.scalar_one_or_none()
         
     
     async def insert_many_chunks(self, chunks: list[DataChunk], batch_size: int=100) -> int:
@@ -41,9 +41,8 @@ class ChunkModel(BaseDataModel):
                     batch = chunks[i:i+batch_size]
                     session.add_all(batch)
             await session.commit()
-            return len(chunks)
+        return len(chunks)
             
-
     async def delete_chunks_by_project_id(self, project_id: ObjectId) -> int:
         async with self.db_client() as session:
             stmt = delete(DataChunk).where(DataChunk.chunk_project_id == project_id)
@@ -51,11 +50,17 @@ class ChunkModel(BaseDataModel):
             await session.commit()
         return result.rowcount
     
-    
     async def get_project_chunks(self, project_id: ObjectId, page_no: int=1, page_size:int=50):
         async with self.db_client() as session:
             stmt = select(DataChunk).where(DataChunk.chunk_project_id == project_id).offset((page_no - 1) * page_size).limit(page_size)
             result = await session.execute(stmt)
             records = result.scalars().all()
-            return records
+        return records
         
+    async def get_total_chunks_count(self, project_id: ObjectId) -> int:
+        total_count = 0
+        async with self.db_client() as session:
+            sql_count = select(func.count(DataChunk.chunk_id)).where(DataChunk.chunk_project_id == project_id)
+            result = await session.execute(sql_count)
+            total_count = result.scalar_one()
+        return total_count 
