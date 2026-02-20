37af25717e3124d04ac120ab2a210a8c5800e1e6
AlharthAlhajHussein <alharth.alhaj.hussein@gmail.com>
2026-02-20 22:40:12 +0300

    Implement PgVectorDBProvider for PostgreSQL integration; update vector DB provider factory to support new provider

--- src/stores/vector_db/vector_db_provider_factory.py
+++ src/stores/vector_db/vector_db_provider_factory.py
@@ -1,19 +1,33 @@
-from .providers import QdrantDBProvider
+from .providers import QdrantDBProvider, PgVectorDBProvider
 from .vector_db_enums import VectorDBType
 from controllers import BaseController
-
+from sqlalchemy.orm import sessionmaker
 
 class VectorDBProviderFactory:
     
-    def __init__(self, config):
+    def __init__(self, config, db_client: sessionmaker = None):
         self.config = config
         self.base_controller = BaseController()
+        self.db_client = db_client
     
     def create(self, provider: str):
         if provider == VectorDBType.QDRANT.value:
-            db_path = self.base_controller.get_database_path(self.config.VECTOR_DB_PATH)
-            distance = self.config.VECTOR_DB_DISTANCE_METRIC
-            return QdrantDBProvider(db_path=db_path, distance=distance)
+            qdrant_db_client = self.base_controller.get_database_path(self.config.VECTOR_DB_PATH)
+            return QdrantDBProvider(
+                db_client=qdrant_db_client, 
+                default_distance_method=self.config.VECTOR_DB_DISTANCE_METRIC,
+                default_vector_dimension=self.config.EMBEDDING_SIZE,
+                index_threshold=self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD
+            )
+            
+        if provider == VectorDBType.PGVECTOR.value:
+            return PgVectorDBProvider(
+                db_client=self.db_client, 
+                default_distance_method=self.config.VECTOR_DB_DISTANCE_METRIC,
+                default_vector_dimension=self.config.EMBEDDING_SIZE,
+                index_threshold=self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD
+            )
+            
         else:
             raise ValueError(f"Unsupported vector DB provider: {provider}")
         
